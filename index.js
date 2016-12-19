1d037eae474b9517f02e09d5d079c5cda8d46798
Tim Squires <antiockus@gmail.com>
2016-12-19 14:24:37 -0500

    added caster info, dynamic caster list

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
+var currentFaction = '';
+var currentCaster = '';
+
 (function($) {
+    var jsonData;
+    var casterInfo;
 
     $.getJSON('factions.json', function(data) {
+        jsonData = data.factions;
         $.each(data.factions, function(ind, dat) {
-            $('.column-1').append('<div class="faction-box"><h2>' + dat + '</h2></div>');
+            $('.column-1').append('<div class="faction-box"><h2>' + ind + '</h2></div>');
         });
     });
 
+    $.getJSON('casters.json', function(data) {
+        casterInfo = data;
+        // $.each(casterInfo, function(dat, ind) {
+        //     console.log(ind.caster_one.name);
+        // })
+    });
+
     $('.container').on('click', '.faction-box', function(e) {
-        $('.active').removeClass('active-faction');
+        $('.active-faction').removeClass('active-faction');
         $(this).toggleClass('active-faction');
-        console.log($(this).text());
+        currentFaction = $(this).text();
+        $('.caster-list').empty();
+        $.each(jsonData[currentFaction], function(ind, dat) {
+            $('.caster-list').append('<div class="caster-box"><h2>' + dat + '</h2></div>');
+        });
+    });
+
+    $('.container').on('click', '.caster-box', function() {
+        currentCaster = $(this).text();
+        console.log(currentCaster);
+
     });
 
 })(jQuery)
